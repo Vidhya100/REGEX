14b619b61f6e3478c8324d07ab062c7d6855376a
Vidhya100 <vidhyadarade1@gmail.com>
2022-08-17 18:47:10 +0530

    Firt name checked

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace RegexDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the First Name ");
+            string pattern = Console.ReadLine();
+
+            //Pattern patterns = new Pattern();
+            //bool val = patterns.validatePinCode(pattern);
+
+            //Pattern patterns = new Pattern();
+            //bool val = patterns.validatePhoneNumb(pattern);
+
+            UserRegi user = new UserRegi();
+            bool val = user.UserRegistration(pattern);
+
+            if (val)
+            {
+                Console.WriteLine("Pattern Matched");
+            }
+            else
+            {
+                Console.WriteLine("Pattern Not Matched");
+            }
+        }
+    }
+}
---
+++ UserRegi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexDemo
+{
+    public class UserRegi
+    {
+        public static string USER_FIRST_NAME = "^[A-Z]{1}[A-Za-z]{2,}$";
+        
+
+        public bool UserRegistration(string name)
+        {
+            return Regex.IsMatch(name, USER_FIRST_NAME);  
+        }
+    }
+}
