08bff8a2f0f1ac8094162f09f50862d6e1a65ea0
Vidhya100 <vidhyadarade1@gmail.com>
2022-08-17 19:21:03 +0530

    Last name checked

--- Program.cs
+++ Program.cs
@@ -4,19 +4,26 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Please enter the First Name ");
-            string pattern = Console.ReadLine();
+            //Console.WriteLine("Please enter the First Name ");
+            //string firstname = Console.ReadLine();
 
-            //Pattern patterns = new Pattern();
-            //bool val = patterns.validatePinCode(pattern);
-
-            //Pattern patterns = new Pattern();
-            //bool val = patterns.validatePhoneNumb(pattern);
 
             UserRegi user = new UserRegi();
-            bool val = user.UserRegistration(pattern);
+            //bool val = user.FirstName(firstname);
 
-            if (val)
+            //if (val)
+            //{
+            //    Console.WriteLine("Pattern Matched");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Pattern Not Matched");
+            //}
+
+            Console.WriteLine("Please enter the Last Name ");
+            string lastname = Console.ReadLine();
+            bool val1 = user.LastName(lastname);
+            if (val1)
             {
                 Console.WriteLine("Pattern Matched");
             }
--- UserRegi.cs
+++ UserRegi.cs
@@ -10,12 +10,15 @@ namespace RegexDemo
     public class UserRegi
     {
         public static string USER_FIRST_NAME = "^[A-Z]{1}[A-Za-z]{2,}$";
-        //public static string USER_LAST_NAME = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public static string USER_LAST_NAME = "^[A-Z]{1}[A-Za-z]{2,}$";
 
         public bool FirstName(string firstName)
         {
             return Regex.IsMatch(firstName, USER_FIRST_NAME);              
         }
-       
+        public bool LastName(string lastName)
+        {
+            return Regex.IsMatch(lastName, USER_LAST_NAME);
+        }
     }
 }
