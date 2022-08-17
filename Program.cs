8a3c1202fcd9effb10fa3204720d372492212848
Vidhya100 <vidhyadarade1@gmail.com>
2022-08-18 00:32:20 +0530

    Paasword with at least 1 digit characters checked

--- Program.cs
+++ Program.cs
@@ -68,7 +68,7 @@
             }
             else
             {
-                Console.WriteLine("Pattern Not Matched, there must be 8 characters and at least 1 uppercase character.");
+                Console.WriteLine("Pattern Not Matched, there must be 8 characters and at least 1 uppercase, 1 digit in character.");
             }
 
         }
