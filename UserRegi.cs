a32dfa3c81ba83fd2c0ab4babacde6a374be0612
Vidhya100 <vidhyadarade1@gmail.com>
2022-08-18 16:53:57 +0530

    Email checked

--- UserRegi.cs
+++ UserRegi.cs
@@ -9,10 +9,11 @@ namespace RegexDemo
 {
     public class UserRegi
     {
-        public static string USER_FIRST_NAME = "^[A-Z]{1}[A-Za-z]{2,}$";
-        public static string USER_LAST_NAME = "^[A-Z]{1}[A-Za-z]{2,}$";
-        public static string USER_EMAIL = "^[a-z0-9A-Z]+[.+-_]{0,1}[0-9a-zA-Z]{0,}+[@][a-zA-Z]+[.][a-zA-z]{2,3}([.][a-zA-z]{2,3}){0,1}";
-        
+        public static string USER_FIRST_NAME = "^[A-Z]{1}[A-Za-z]{3,20}$";
+        public static string USER_LAST_NAME = "^[A-Z]{1}[A-Za-z]{3,20}$";
+        public static string USER_EMAIL = @"^[A-Za-z0-9]{3,}([\.\-\+][A-Za-z0-9]{3,})?[@][a-zA-Z0-9]{1,}[.][a-zA-Z]{2,}([.][a-zA-Z]{2,})?$";
+        public static string MOBILE_NUMBER = "^([+][0-9]{2}[ ]){0,1}[6-9]{1}[0-9]{9}$";
+        public static string PASSWORD = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$^&+=?!])[0-9a-zA-Z]{8,}$";
         public bool FirstName(string firstName)
         {
             return Regex.IsMatch(firstName, USER_FIRST_NAME);              
@@ -24,6 +25,14 @@ namespace RegexDemo
         public bool Email(string email)
         {
             return Regex.IsMatch(email, USER_EMAIL);
+        }
+        public bool MobileNumber(string mobileNumb)
+        {
+            return Regex.IsMatch(mobileNumb, MOBILE_NUMBER);
+        }
+        public bool Password(string password)
+        {
+            return Regex.IsMatch(password, PASSWORD);
         }
     }
 }
